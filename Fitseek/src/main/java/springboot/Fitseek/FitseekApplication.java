fa3123d33531eb39c2348cb060a6054464bda8cb
Yushuishen <shuishenyu@gmail.com>
2025-04-21 16:19:09 +0800

    初始化项目文件夹
    初始化项目文件夹

---
+++ Fitseek/src/main/java/springboot/Fitseek/FitseekApplication.java
@@ -0,0 +1,13 @@
+package springboot.Fitseek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitseekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitseekApplication.class, args);
+	}
+
+}
---
+++ Fitseek/src/test/java/springboot/Fitseek/FitseekApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.Fitseek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitseekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
